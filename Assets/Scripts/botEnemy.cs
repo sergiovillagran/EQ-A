a8960661d0443ed1f067c64c8ed93aa448fb0131
Adrian Vargas <adrianm.1218@hotmail.com>
2017-01-31 15:43:13 -0500

    Integrando Assets.

---
+++ Assets/Scripts/botEnemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botEnemy : MonoBehaviour {
+    public GameObject Missile;
+
+    void Start () {
+        InvokeRepeating("shootMissile", 3.0f, 3.0f);
+    }
+	
+	void Update () {
+		
+	}
+
+    public void shootMissile ()
+    {
+        Instantiate(Missile);
+    }
+}
---
+++ Assets/Scripts/homingMissile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class homingMissile : MonoBehaviour {
+
+    public float speed = 5;
+    public float rotatingSpeed = 200;
+
+    GameObject target;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+
+        Vector2 point2Target = (Vector2)transform.position - (Vector2)target.transform.position;
+        point2Target.Normalize();
+        float value = Vector3.Cross(point2Target, transform.right).z;
+
+        rb.angularVelocity = rotatingSpeed * value;
+        rb.velocity = transform.right * speed;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag != "deadly")
+        {
+            Destroy(this.gameObject, 0.02f);
+        }
+    }
+}
